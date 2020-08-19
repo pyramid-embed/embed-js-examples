8982a81737b53fff3e5addfe7348ed9833353621
Itamar Held <itamar.held@pyramidanalytics.com>
2020-08-19 19:13:14 +0300

    add inquirer and promisify

--- run.js
+++ run.js
@@ -1,76 +1,75 @@
-const copydir = require('copy-dir');
-const rimraf = require('rimraf');
+const { promisify } = require('util');
+const copydir = promisify(require('copy-dir'));
+const rimraf = promisify(require('rimraf'));
 const server = require('node-http-server');
-const commandLineArgs = require('command-line-args')
+const commandLineArgs = require('command-line-args');
 const replaceInFiles = require('replace-in-files');
 const opn = require('opn');
+const inquirer = require('inquirer');
 
-const optionDefinitions = [{
-        name: 'username',
-        alias: 'u',
-        type: String
-    },
-    {
-        name: 'password',
-        alias: 'p',
-        type: String
-    },
-    {
-        name: 'host',
-        alias: 'h',
-        type: String
-    },
-    {
-        name: 'port',
-        alias: 'o',
-        type: Number
-    },
-    {
-        name: 'content',
-        alias: 'c',
-        type: String
-    }
-]
+const optionDefinitions = [
+	{
+		name: 'port',
+		alias: 'o',
+		type: Number,
+	},
+	{
+		name: 'content',
+		alias: 'c',
+		type: String,
+	},
+];
 
 const options = commandLineArgs(optionDefinitions);
-const port = options.port || 8000;
 
+run();
 
-console.log('Removing dist folder...');
-rimraf('./dist', () => {
-    console.log('Removed.');
+async function run() {
+	const { content, port = 8000 } = options;
+	const { username, password, host } = await inquirer.prompt([
+		{ type: 'input', name: 'username', message: 'User Name:' },
+		{ type: 'password', name: 'password', message: 'Password:', mask: '*' },
+		{ type: 'input', name: 'host', message: 'Host:' },
+	]);
 
-    console.log('Copy files...');
-    copydir('./src', './dist', async () => {
-        console.log('Copied');
+	console.log('Removing dist folder...');
+	await rimraf('./dist');
 
-        console.log('Applying parameters');
+	console.log('Removed.');
 
-        await replaceInFiles({
-                files: './dist/**/*.*',
-                from: /<USERNAME>/g,
-                to: options.username,
-            })
-            .pipe({
-                from: /<PASSWORD>/g,
-                to: options.password
-            })
-            .pipe({
-                from: /<HOST>/g,
-                to: options.host
-            })
-            .pipe({
-                from: /<CONTENT>/g,
-                to: options.content
-            })
+	console.log('Copy files...');
+	await copydir('./src', './dist');
+	console.log('Copied');
 
-        console.log('Deploying server...')
-        server.deploy({
-            port: port,
-            root: './dist',
-        }, () => {
-            console.log('Listen, Port ' + port);
-            opn('http://localhost:' + port);
-        });
-    });
-});
+	console.log('Applying parameters');
+
+	await replaceInFiles({
+		files: './dist/**/*.*',
+		from: /<USERNAME>/g,
+		to: username,
+	})
+		.pipe({
+			from: /<PASSWORD>/g,
+			to: password,
+		})
+		.pipe({
+			from: /<HOST>/g,
+			to: host,
+		})
+		.pipe({
+			from: /<CONTENT>/g,
+			to: content,
+		});
+
+	console.log('Deploying server...');
+	server.deploy(
+		{
+			port: port,
+			root: './dist',
+		},
+		() => {
+			console.log('Listen, Port ' + port);
+			opn('http://localhost:' + port);
+		}
+	);
+}
